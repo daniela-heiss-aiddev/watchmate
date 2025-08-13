6ede82b0e1185a84ce168e21de47db900f9d9c54
Daniela Heiß <daniela.heiss@aiddevs.com>
2025-08-13 10:35:45 +0200

    Exchanged movies for watchlist

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 #from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import MovieListAV, MovieDetailAV
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV
 
 urlpatterns = [
-    path('list/', MovieListAV.as_view(), name='movie-list'),
-    path('<int:pk>', MovieDetailAV.as_view(), name='movie-details'),
+    path('list/', WatchListAV.as_view(), name='watch-list'),
+    path('<int:pk>', WatchListDetailAV.as_view(), name='watch-list-details'),
 ]
