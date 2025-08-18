711efe15ba95da491e8f3c1febe99dea3b75122c
Daniela Heiß <daniela.heiss@aiddevs.com>
2025-08-18 15:11:28 +0200

    Added user to review

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,18 +1,30 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 #from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import viewsets
 from watchlist_app.api.serializers import WatchlistSerializer, StreamingPlatformSerializer, ReviewSerializer
 from watchlist_app.models import WatchList, StreamingPlatform, Review
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
     def perform_create(self, serializer):
         movie_id = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=movie_id)
-        serializer.save(watchlist=movie)
+
+        review_user = self.request.user
+
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie.")
+ 
+        serializer.save(watchlist=movie, review_user=review_user)
 
 class ReviewList(generics.ListAPIView):
     serializer_class = ReviewSerializer
@@ -81,6 +93,17 @@ class WatchListDetailAV(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 ### Streaming Platform###
+
+# class StreamingPlatform(viewsets.ViewSet):
+#     def list(self, request):
+#         querySet = StreamingPlatform.objects.all()
+#         serializer = StreamingPlatformSerializer(querySet, many=True)
+#         return Response(serializer.data)
+#     def retrieve(self, request, pk=None):
+#         querySet = StreamingPlatform.objects.get(pk=pk)
+#         platform = get_object_or_404(StreamingPlatform, pk=pk)
+#         serializer = StreamingPlatformSerializer(platform)
+#         return Response(serializer.data)
 
 class StreamingPlatformListAV(APIView):
     def get(self, request):
