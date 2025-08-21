054a340f05c5b793732a9acda36fd5294f9d9478
Daniela Heiß <daniela.heiss@aiddevs.com>
2025-08-21 10:23:54 +0200

    Fixed url issue

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -5,7 +5,7 @@ from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamingPla
 urlpatterns = [
     ###Watchlist API URLs###
     path('list/', WatchListAV.as_view(), name='movie-list'),
-    path('<int:pk/>', WatchListDetailAV.as_view(), name='movie-list-details'),
+    path('<int:pk>/', WatchListDetailAV.as_view(), name='movie-list-details'),
     ###Streaming Platform API URLs###
     path('stream/', StreamingPlatformListAV.as_view(), name='streamingplatform-list'),
     path('stream/<int:pk>/', StreamingPlatformDetailAV.as_view(), name='streamingplatform-detail'),
